c2486a2dd575e9a0b8f8eebef219ea40906c2d01
LexusIgnatenko <lexus24081985@gmail.com>
2025-08-13 11:32:48 +0300

    1

---
+++ src/js/app.js
@@ -0,0 +1,24 @@
+import Bowerman from './Classes/Bowerman.js';
+import Daemon from './Classes/Daemon.js';
+import Magician from './Classes/Magician.js';
+import Swordsman from './Classes/Swordsman.js';
+import Undead from './Classes/Undead.js';
+import Zombie from './Classes/Zombie.js';
+
+const bowman = new Bowerman('Archy');
+console.log(bowman);
+
+const daemon = new Daemon('Zazu');
+console.log(daemon);
+
+const magician = new Magician('Angel');
+console.log(magician);
+
+const swordsman = new Swordsman('Viktor');
+console.log(swordsman);
+
+const undead = new Undead('Teddy');
+console.log(undead);
+
+const zombie = new Zombie('Boo');
+console.log(zombie); 
